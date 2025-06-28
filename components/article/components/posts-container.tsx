bcc82865a76406683c2bc7b4d92e2c93e0d6a200
Yaniv Avraham <yanivemail5@gmail.com>
2025-06-28 17:09:02 +0300

    fix /articles

--- components/article/components/posts-container.tsx
+++ components/article/components/posts-container.tsx
@@ -6,9 +6,9 @@ import { Post } from "@/lib/types";
 export function PostsContainer({ posts }: { posts: Post[] }) {
   return (
     <div className="space-y-6">
-      <ul className="list-none">
+      <ul className="list-none grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {posts.map((post) => (
-          <li key={post.id}>
+          <li key={post.id} className="col-span-1 ">
             <PostCard post={post} />
           </li>
         ))}
